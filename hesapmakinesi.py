fee9f2fab985654a014e51daf0cb0729b358341b
dmizginn <80034008+dmizginn@users.noreply.github.com>
2022-12-18 20:24:03 +0300

    Ucuncu yukleme

--- hesapmakinesi.py
+++ hesapmakinesi.py
@@ -3,7 +3,12 @@ sayi1= int(input("Bir sayı giriniz :"))
 sayi2= int(input("İkinci sayı giriniz :"))
 toplam= sayi1 + sayi2
 cikartma= sayi1 - sayi2
+carpma= sayi1 * sayi2
+bolme= sayi1 / sayi2
+
 
 
 print("Toplama işlemi : " + str( toplam ))
 print("Cikartma işlemi : " + str( cikartma ))
+print("Carpma işlemi : " + str( carpma ))
+print("Bolme işlemi : " + str( bolme ))
