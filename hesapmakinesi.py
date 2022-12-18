a9eea186cc4ff17b1fb1f2bcee97e465432f72d4
dmizginn <80034008+dmizginn@users.noreply.github.com>
2022-12-18 20:18:50 +0300

    cikartma islemi eklendi

--- hesapmakinesi.py
+++ hesapmakinesi.py
@@ -2,6 +2,8 @@
 sayi1= int(input("Bir sayı giriniz :"))
 sayi2= int(input("İkinci sayı giriniz :"))
 toplam= sayi1 + sayi2
+cikartma= sayi1 - sayi2
 
 
 print("Toplama işlemi : " + str( toplam ))
+print("Cikartma işlemi : " + str( cikartma ))
